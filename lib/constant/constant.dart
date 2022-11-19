db67e07252cb3051dae9c32da70a71b901f46ea4
Sriram <srivishal600@gmail.com>
2022-11-19 12:45:33 +0530

    Created custom widget for the column

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -5,3 +5,6 @@ const a2 = AssetImage("assets/2.png");
 const a3 = AssetImage("assets/3.png");
 const a4 = AssetImage("assets/4.png");
 
+const grey = Color(0xFF767676);
+const rec_color = Color(0xFFE6EFF8);
+const black = Colors.black;
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -61,26 +61,128 @@ class _HomePageState extends State<HomePage> {
             // ),
 
 
-            Container(
+             Container(
               height: 300,
               width: double.infinity,
               color: Colors.red,
+               child: Center(
+                 child: CircularProgressIndicator(),
+               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Container(
+
+                margin: EdgeInsets.only(left: 20,right: 20,top: 320),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+               //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("CCT/55/5501",
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Quicksand',
+                        ),),
+                      SizedBox(height: 10,),
+                      Row(
+                    //  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                           Image(
+                               image: AssetImage("assets/icon/1.png"),
+                             width: 20,
+                             height: 20,
+                           ),
+                          SizedBox(width: 5,),
+                          Text("Residential Apartments",
+                          style: TextStyle(
+                            fontFamily: "Quicksand",
+                            fontSize: 14
+                          ),),
+                          SizedBox(width: 10,),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: grey,
+                            ),
+                            height: 10,
+                            width: 10,
+                          ),
+                          SizedBox(width: 5,),
+                          Text("Premium Sea View",
+                            style: TextStyle(
+                                fontFamily: "Quicksand",
+                                fontSize: 14
+                            ),),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Text("AED 850,000",
+                        style: TextStyle(
+                            fontFamily: "Quicksand",
+                            fontSize: 24,
+                          fontWeight: FontWeight.bold
+                        ),),
+                      SizedBox(height: 10,),
+                      Text("Discover \"SKYZ by Danube\" Living",
+                        style: TextStyle(
+                            fontFamily: "Quicksand",
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                        ),),
+                      SizedBox(height: 15,),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: rec_color,
+                        ),
+                      //  foregroundDecoration: ,
+                        height: 128,
+                        width: 125,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image(image: AssetImage("assets/icon/icon_bed.png"),
+                            width: 50,
+                            height: 50,),
+                            RichText(
+                              text : TextSpan(
+                              children: const <TextSpan>[
+                                TextSpan(text: '1',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    fontFamily: "Quicksand",
+                                    fontSize: 18,
+                                    color: black,
+                                  ),
+                                ),
+                                TextSpan(text: ' Bedroom',
+                                  style: TextStyle(
+                                    fontFamily: "Quicksand",
+                                    fontSize: 18,
+                                    color: black,
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                            ),
+
+
+                          ],
+
+                        ),
+
+                      ),
+
+                    ],
+                  ),
+                ),
             ),
 
+
           ],
-            Container(
-              margin: EdgeInsets.only(left: 10,right: 10),
-              child: Column(
-                children: [
-                  Text("CCT/55/5501",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.normal,
-                      fontFamily: 'Quicksand',
-                    ),)
-                ],
-              ),
-            ),
+
         ),
       ),
     );
